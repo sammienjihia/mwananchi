299dcc5b7e7adc4dcbd648d6b0a995e021865bde
sammienjihia <dilaninjihia@gmail.com>
2016-11-03 17:25:01 +0300

    Basically minimized the work load performed by searchwordview function by incoporaring the getusersearchword function

--- search/views.py
+++ search/views.py
@@ -10,98 +10,50 @@ import json
 
 from searchfunction import searchingfunction
 from searchfunction import tweetcount
+from searchfunction import postweets, negtweets, neutweets, getusersearchword
 from django.shortcuts import redirect
 
 
 
 
-# def topic_to_keywords(request):
-#     topic = request.GET.get('topic')
-#     ret = []
-#     title = "Search"
-#     template = loader.get_template('search/search.html')
-#     form = CascadeForm(request.GET or None)
-#     context = {"form": form, "title": title}
-#
-#     if topic:
-#         for keywords in KeyWords.objects.filter(topic_id=topic):
-#             ret.append(dict(id=keywords.key_word_id, value=unicode(keywords)))
-#     if len(ret)!=1:
-#         ret.insert(0, dict(id='', value='---'))
-#
-#     return HttpResponse(simplejson.dumps(ret))
 
-
-
-
-
-
-
-def searchwordview(request):
+def searchwordview(newsearchword1):
     global jsonData
     jsonData = []
     title = "Search"
     template = loader.get_template('search/search.html')
-    if request.method == 'GET':
-        form = SearchForm()
-
-    else:
-        form = SearchForm(request.POST)
-
-        if form.is_valid():
-            newsearchword = request.POST['searchword']
-
-            try:
-                table = Datasheet.load(pd("sammy.csv"))
-                index = set(table.columns[0])
-
-            except:
-                table = Datasheet()
-                index = set()
-
-            engine = Twitter(language="en")
-
-            prev = None
-
-            for i in range(2):
-                print i
-
-                for tweet in engine.search(newsearchword, start=prev, count=10, cached=False):
-                    pol = polarity(tweet.text)
-                    print
-                    print tweet.text
-                    print tweet.author
-                    print tweet.date
-                    print hashtags(tweet.text)
-                    print sentiment(tweet.text)
-                    print pol
-                    print
-
-                    global newdata
-                    jsonData.append({'text':tweet.text, 'author':tweet.author, 'date':tweet.date, 'hashtags':hashtags(tweet.text),
-                                     'sentiments':sentiment(tweet.text), 'polarity':polarity(tweet.text)})
-
-                    if len(table) == 0 or tweet.id not in index:
-                        table.append([tweet.id, tweet.text])
-                        index.add(tweet.id)
-
-                    prev = tweet.id
-
-            #table.save(pd("sammy.csv"))
-
-            print "Total results:  ", len(table)
-            print
-            #return (jsonData)
-        #return HttpResponse(json.dumps(jsonData))
+    form = SearchForm()
+    searchingword = getusersearchword(newsearchword1)
+    print searchingword
+    if searchingword:
         return redirect("results/")
+    else:
+        print "getusersearchword() returned an empty object"
+    # if request.method == 'GET':
+    #     form = SearchForm()
+    #
+    # else:
+    #     form = SearchForm(request.POST)
+    #
+    #     if form.is_valid():
+    #         newsearchword = request.POST['searchword']
+        #searchingfunction(newsearchword)
+
+        #return redirect("results/")
 
 
     context = {"form": form, "title": title}
-    return HttpResponse( template.render(context, request))
+    return HttpResponse( template.render(context, newsearchword1))
 
-def results(jsonData1):
-    jsonData2 =searchingfunction(jsonData1)
+def results(newsearchword):
+
+    jsonData2 = searchingfunction(newsearchword)
+    print "sammy"
+
+    postweets2 = postweets(postweets)
+    negtweets2 = negtweets(negtweets)
+    neutweets2 = neutweets(neutweets)
     tweetcount3 = tweetcount(jsonData2)
     template = loader.get_template('search/results.html')
-    context = { "newdata":jsonData2, "tweetcount":tweetcount3}
+    context = { "newdata":jsonData2, "tweetcount":tweetcount3, "postweets": postweets2, "negtweets": negtweets2, "neutweets": neutweets2}
     return HttpResponse( template.render(context))
