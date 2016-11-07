3b9a81aa29bc62e9b3b48ef2237a8728e60b51e7
sammienjihia <dilaninjihia@gmail.com>
2016-11-08 00:29:41 +0300

    Added a couple of functions

--- sms/sendtext.py
+++ sms/sendtext.py
@@ -1,11 +1,12 @@
 # Be sure to import the helper gateway class
 from africastalking.AfricasTalkingGateway import AfricasTalkingGateway, AfricasTalkingGatewayException
 from pattern.en import sentiment, polarity, subjectivity, positive
-
+from models import Insms
 
 def receivedsms(message1):
 
     message1 = []
+    newmessage1 = []
 
     username = "karanjaeric"
     apikey = "54e6934ca4fb2b2f675fc2534246719efb37389d5341a590d6c3a042f4d83275"
@@ -29,8 +30,10 @@ def receivedsms(message1):
                 #this is for test purposes
                 keyword = message['text'].split()
                 message1.append({'from': message['from'], 'to': message['to'], 'date': message['date'], 'text': message['text'],'polarity': polarity(message['text']), 'keyword':keyword[0]})
-
+                obj, created = Insms.objects.get_or_create(sender=message['from'], to=message['to'], date=message['date'],
+                                                           text=message['text'], polarity=polarity(message['text']), keyword=keyword[0])
                 print keyword[0]
+                print created
                 lastReceivedId = message['id']
             if len(messages) == 0:
                 break
@@ -39,3 +42,29 @@ def receivedsms(message1):
     except AfricasTalkingGatewayException, e:
         print 'Encountered an error while fetching messages: %s' % str(e)
     return (message1)
+
+
+def smscount(smsCount):
+    smsCount2 = len(smsCount)
+    return (smsCount2)
+
+def posisms(messages1):
+    posisms = 0
+    for sms in messages1:
+        if sms.polarity > 0:
+            posisms +=1
+    return (posisms)
+
+def negsms(messages1):
+    negsms = 0
+    for sms in messages1:
+        if sms.polarity < 0:
+            negsms +=1
+    return (negsms)
+
+def neutsms(messages1):
+    neutsms = 0
+    for sms in messages1:
+        if sms.polarity == 0:
+            neutsms +=1
+    return (neutsms)
